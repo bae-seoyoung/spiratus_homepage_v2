4e51fd3b425dfe34c76430682851658994f57be5
Bailey <bsy2658@gmail.com>
2023-12-05 18:53:22 +0900

    투명한 appbar와 메뉴 아이콘 추가

--- lib/main.dart
+++ lib/main.dart
@@ -33,56 +33,65 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-        body: Center(
-          child: Container(
-            width: double.infinity,
-            height: double.infinity,
-            child: Stack(
-              children: [
-                Image.asset(
-                  'assets/main_background.jpg',
-                  fit: BoxFit.cover,
-                  width: double.infinity,
-                  height: double.infinity,
+      extendBodyBehindAppBar: true, // AppBar를 body 영역 뒤로 확장
+      appBar: AppBar(
+        backgroundColor: Colors.transparent, // 투명한 AppBar 배경색
+        elevation: 0, // 그림자 효과 제거
+        title: Text(''), // 타이틀을 비워서 공간 차지하지 않게
+        actions: [
+          IconButton(
+            icon: Icon(Icons.menu),
+            onPressed: () {
+              // TODO: 메뉴 아이콘을 눌렀을 때 수행할 동작 추가
+              print('메뉴 아이콘을 눌렀습니다.');
+            },
+          ),
+        ],
+      ),
+      body: Center(
+        child: Container(
+          width: double.infinity,
+          height: double.infinity,
+          child: Stack(
+            children: [
+              Image.asset(
+                'assets/main_background.jpg',
+                fit: BoxFit.cover,
+                width: double.infinity,
+                height: double.infinity,
+              ),
+              Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'SPIRATUS',
+                      style: GoogleFonts.ysabeauInfant( // GoogleFonts를 사용하여 Ysabeau Infant 폰트 가져오기
+                          fontWeight: FontWeight.w200,
+                          fontSize: 40.0,
+                          letterSpacing: 20,
+                          color: Colors.black,
+                          height: 2.0,
+                        ),
+                    ),
+                    Text(
+                      'MEDITATION | CONNECTEDNESS | EMBODIMENT',
+                      style: GoogleFonts.ysabeauInfant( // GoogleFonts를 사용하여 Ysabeau Infant 폰트 가져오기
+                          fontWeight: FontWeight.w200,
+                          fontSize: 20.0,
+                          letterSpacing: 1.0,
+                          color: Colors.black,
+                        ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
                 ),
-                Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        'SPIRATUS',
-                        style: GoogleFonts.ysabeauInfant( // GoogleFonts를 사용하여 Ysabeau Infant 폰트 가져오기
-                            fontWeight: FontWeight.w200,
-                            fontSize: 40.0,
-                            letterSpacing: 20,
-                            color: Colors.black,
-                            height: 2.0,
-                          ),
-                      ),
-                      Text(
-                        'MEDITATION | CONNECTEDNESS | EMBODIMENT',
-                        style: GoogleFonts.ysabeauInfant( // GoogleFonts를 사용하여 Ysabeau Infant 폰트 가져오기
-                            fontWeight: FontWeight.w200,
-                            fontSize: 20.0,
-                            letterSpacing: 1.0,
-                            color: Colors.black,
-                          ),
-                        textAlign: TextAlign.center,
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
-      );
+      ),
+    );
   }
 }
