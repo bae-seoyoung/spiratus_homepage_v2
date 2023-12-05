d4865eed5953db34344a26433a0945a6ade99709
Bailey <bsy2658@gmail.com>
2023-12-05 19:10:53 +0900

    메뉴 아이콘 누르면 drawer가 나오도록 수정

--- lib/main.dart
+++ lib/main.dart
@@ -30,24 +30,29 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  // GlobalKey를 사용하여 ScaffoldState에 접근하기 위한 키 생성
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey, // ScaffoldState에 접근하기 위해 키 설정
       extendBodyBehindAppBar: true, // AppBar를 body 영역 뒤로 확장
       appBar: AppBar(
         backgroundColor: Colors.transparent, // 투명한 AppBar 배경색
         elevation: 0, // 그림자 효과 제거
         title: Text(''), // 타이틀을 비워서 공간 차지하지 않게
+        leading: Container(), // 좌측 상단에 있는 아이콘을 없애는 부분
         actions: [
-          IconButton(
-            icon: Icon(Icons.menu),
-            onPressed: () {
-              // TODO: 메뉴 아이콘을 눌렀을 때 수행할 동작 추가
-              print('메뉴 아이콘을 눌렀습니다.');
-            },
-          ),
-        ],
+            // 아이콘을 추가할 부분
+            IconButton(
+              icon: Icon(Icons.menu),
+              onPressed: () {
+                // Drawer를 열기 위해 ScaffoldState의 openDrawer 메서드 호출
+                _scaffoldKey.currentState!.openDrawer();
+              },
+            ),
+          ],
       ),
       body: Center(
         child: Container(
@@ -90,6 +95,40 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ],
           ),
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text(
+                '메뉴',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              title: Text('메뉴 항목 1'),
+              onTap: () {
+                // TODO: 메뉴 항목 1을 선택했을 때 수행할 동작 추가
+                Navigator.pop(context); // Drawer를 닫음
+              },
+            ),
+            ListTile(
+              title: Text('메뉴 항목 2'),
+              onTap: () {
+                // TODO: 메뉴 항목 2를 선택했을 때 수행할 동작 추가
+                Navigator.pop(context); // Drawer를 닫음
+              },
+            ),
+            // 추가적인 메뉴 항목들을 필요에 따라 추가
+          ],
         ),
       ),
     );
