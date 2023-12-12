270c3d9bdcfb54b56a89b3bff96ac64459e4a927
Bailey <bsy2658@gmail.com>
2023-12-13 01:48:31 +0900

    컨텍트 페이지 구성

--- lib/contact.dart
+++ lib/contact.dart
@@ -5,10 +5,102 @@ class ContactPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Contact Page'),
+        backgroundColor: Colors.transparent, // 투명한 AppBar 배경색
+        toolbarHeight: MediaQuery.of(context).size.height * 0.2,
+        elevation: 0, // 그림자 효과 제거
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+            image: AssetImage('assets/main_background.jpg'), // 이미지 파일 경로로 수정
+            fit: BoxFit.cover,
+            ),
+          ),
+        ),
+      title: Text(
+        'CONTACT',
+        style: TextStyle(
+          fontWeight: FontWeight.w100,
+          fontSize: 30.0,
+          letterSpacing: 20,
+          color: Colors.black,
+        ),
       ),
-      body: Center(
-        child: Text('This is the Contact Page'),
+      ),
+      body: SingleChildScrollView( // 스크롤을 추가하는 부분
+        child: Padding(
+          padding: const EdgeInsets.all(150.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                '이름(소속):',
+                style: TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'Your Name',
+                ),
+              ),
+              SizedBox(height: 16.0),
+              Text(
+                '메일 주소:',
+                style: TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  hintText: 'Your Email Address',
+                ),
+              ),
+              SizedBox(height: 16.0),
+              Text(
+                '문의 사항:',
+                style: TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SingleChildScrollView(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  minHeight: 100.0, // 원하는 높이를 설정
+                ),
+                child: TextField(
+                  maxLines: null, // 필요한 만큼 자동으로 높아짐
+                  keyboardType: TextInputType.multiline,
+                  decoration: InputDecoration(
+                    hintText: 'Your Message',
+                  ),
+                ),
+              ),
+            ),
+              SizedBox(height: 32.0), // 추가된 여백
+              Center( // 버튼을 화면 중앙에 정렬
+                child: ElevatedButton(
+                  onPressed: () {
+                    // 메일 보내기 또는 제출 기능을 구현할 수 있습니다.
+                  },
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.symmetric(horizontal: 50.0, vertical: 20.0), // 버튼 크기 조절
+                    shape: RoundedRectangleBorder( // 각지게 모양을 위해 RoundedRectangleBorder 사용
+                    borderRadius: BorderRadius.circular(3.0), // 버튼의 각지기 정도를 조절
+                    ),
+                    elevation: 0, // 그림자 효과 제거
+                    primary: Color.fromARGB(255, 150, 193, 199), // 버튼 배경색 설정
+                    onPrimary: Color.fromARGB(255, 0, 0, 0), // 텍스트 색상 설정
+                  ),
+                  child: Text('메일 전송'),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
