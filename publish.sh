53f6844df5615a729d111f58a0b2cbc58746a748
Bailey <bsy2658@gmail.com>
2023-12-12 13:08:15 +0900

    publish 파일 수정

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 flutter pub get
 flutter build web
-aws s3 sync . s3://spiratus.org --profile bae --exclude ".git/*"
+aws s3 sync ./build/web s3://spiratus.org --profile bae --exclude ".git/*"
